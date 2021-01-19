98c740301301bce1802bf2b3bd9f99d5946fbee1
lukemansahid <lukemansahid2020@gmail.com>
2021-01-19 14:35:30 -0100

    Initial commit

---
+++ src/test/java/com/lukish/vehiclehiringsystem/VehicleHiringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukish.vehiclehiringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleHiringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
